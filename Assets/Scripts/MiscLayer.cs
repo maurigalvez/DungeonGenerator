f14d8f3a51b06b80979ab3bd43ba044401424f21
maurigalvez <mauri.galvez@hotmail.com>
2015-02-09 17:34:35 -0500

    no message

--- Assets/Scripts/MiscLayer.cs
+++ Assets/Scripts/MiscLayer.cs
@@ -7,66 +7,189 @@ using System.Collections.Generic;
 [System.Serializable]
 public class MiscLayer : Layer
 {
-	//=================
-	// OBJECT PLACEMENT 
-	//=================
-	public enum sortPlacement
-	{
-		Tshape,
-		Corners,
-		Center,
-        Random,
-	}
-	//=================
-	// VARIABLES
-	//=================
-    public int number;                                  // Number of enemies to be added
-    public string layerName;                            // Name of layer
-    public int id;
-	public GameObject prefab;	                    	// List of objects to be present on mapLayer   
-    //=================
-    // GENERATE
-    //=================
-    public override void generate()
-    {
-        layer = new GameObject(layerName);       
-        this.layerID = id + 1;
+   //=================
+   // OBJECT PLACEMENT 
+   //=================
+   public enum sortPlacement
+   {
+      none,
+      Cross,
+      Corners,
+      Center,
+      Circle,
+      Rectangle,
+   }
+   //=================
+   // VARIABLES
+   //=================
+   public int number;                                   // Number of enemies to be added
+   public string layerName;                             // Name of layer
+   public int id;                                       // Id of MiscLayer
+   public GameObject prefab;	                          // Prefab for tiles of this layer
+   public float radius;                                 // Radius used for circle
+   public sortPlacement distribution;
+   //=================
+   // GENERATE
+   //=================
+   public override void generate()
+   {
+      layer = new GameObject(layerName);       
+      this.layerID = id + 1;
+      //------------------
+      // Generate layer depending on chosen distribution
+      //------------------
+      switch(distribution)
+      {
+         case sortPlacement.Cross:
+            CrossDistribution();
+         break;
 
-        setAtCorners();
+         case sortPlacement.Center:
+            setAtCenter();
+         break;
 
-        setAtCenter();
-    }
+         case sortPlacement.Corners:
+            setAtCorners();
+         break;
 
-    public void setAtCorners()
-    {
-        // bottom left
-        tiles.Add(new Tile(this.x + this.tWidth, this.y + this.tHeight, prefab, this.layerID));
-        // top left
-        tiles.Add(new Tile(this.x + this.tWidth, this.y2 - this.tHeight, prefab, this.layerID));
-        // bottom right
-        tiles.Add(new Tile(this.x2 - this.tWidth, this.y + this.tHeight, prefab, this.layerID));
-        // top right
-        tiles.Add(new Tile(this.x2 - this.tWidth, this.y2 - this.tHeight, prefab, this.layerID));
-    }
+         case sortPlacement.Rectangle:
+            RectangleDistribution();
+         break;
 
-    public void setAtCenter()
-    {
-        // center position
-        float cX = this.x + (this.width * 0.5f) - this.tWidth * 0.5f;
-        float cY = this.y + (this.height * 0.5f) - this.tHeight * 0.5f;
-        // center
-        tiles.Add(new Tile(cX, cY, prefab, this.layerID));
-        // radius
-        float r = 0.75f;
-        float angle = 0.0f;
-        //  
-        while(angle < 270f)
-        {
-            float posX = cX + r * Mathf.Cos(angle);
-            float posY = cY + r * Mathf.Sin(angle);
-            // position tile
-            tiles.Add(new Tile(posX, posY, prefab, this.layerID));
-            angle += 90.0f;
-        }
-    }	
+         case sortPlacement.Circle:
+            CircleDistribution();
+         break;
+
+         default:
+            Debug.LogError("Select a valid sort distribution to generate layer properly!");
+         break;
+      }  
+   }
+   //==================
+   // SET AT CORNERS
+   //==================
+   /// <summary>
+   /// Positions the tiles at the four corners of the room!
+   /// </summary>
+   public void setAtCorners()
+   {
+      // bottom left
+      tiles.Add(new Tile(this.x + this.tWidth, this.y + this.tHeight, prefab, this.layerID));
+      // top left
+      tiles.Add(new Tile(this.x + this.tWidth, this.y2 - this.tHeight, prefab, this.layerID));
+      // bottom right
+      tiles.Add(new Tile(this.x2 - this.tWidth, this.y + this.tHeight, prefab, this.layerID));
+      // top right
+      tiles.Add(new Tile(this.x2 - this.tWidth, this.y2 - this.tHeight, prefab, this.layerID));       
+   }  
+   //================
+   // CIRCLE DISTRIBUTION
+   //================
+   /// <summary>
+   /// Positions the tiles in a circular fashipn at the center of the room
+   /// </summary>
+   public void CircleDistribution()
+   {
+      // center position
+      float cX = this.x + (this.width * 0.5f) - this.tWidth * 0.5f;
+      float cY = this.y + (this.height * 0.5f) - this.tHeight * 0.5f;
+      float angle = 0.0f;
+      //  
+      while(angle < 270f)
+      {
+         float posX = cX + radius * Mathf.Cos(angle);
+         float posY = cY + radius * Mathf.Sin(angle);
+         // position tile
+         tiles.Add(new Tile(posX, posY, prefab, this.layerID));
+         angle += 45.0f;
+      }
+   }	
+   //====================
+   // SET AT CENTER
+   //====================
+   /// <summary>
+   /// Positions one instance of the prefab at the center of the room.
+   /// </summary>
+   public void setAtCenter()
+   {
+      // center position
+      float cX = this.x + (this.width * 0.5f) - this.tWidth * 0.5f;
+      float cY = this.y + (this.height * 0.5f) - this.tHeight * 0.5f;
+      // Add to tiles
+      tiles.Add(new Tile(cX, cY, prefab, layerID));
+   }
+   //======================
+   // RECTANGLE DISTRIBUTION
+   //======================
+   public void RectangleDistribution()
+   {
+      //----------------
+      // Calculate initialX and finalX
+      //----------------
+      float lastX = this.x2 - tWidth * 5f;
+      float cX = this.x + tWidth * 6.25f;
+      float lastY = this.y2 - tHeight * 5f;
+      float cY = this.y + tHeight *5f;
+
+      while(cX < lastX)
+      {
+         tiles.Add(new Tile(cX, cY-tHeight, prefab, layerID));
+         tiles.Add(new Tile(cX, lastY, prefab, layerID));
+         cX += tWidth * 2.0f;
+      }
+      cX = this.x + tWidth * 5f;
+      ///cY _
+      while (cY < lastY)
+      {
+         tiles.Add(new Tile(cX, cY, prefab, layerID));
+         tiles.Add(new Tile(lastX,cY, prefab, layerID));
+         cY += tHeight * 2.0f;
+      }
+   }
+   //====================
+   // CROSS DISTRIBUTION
+   //====================
+   /// <summary>
+   /// Positions prefab in a cross distribution except for the center of the room.
+   /// </summary>
+   public void CrossDistribution()
+   {
+      //-----------------
+      // Calculate middle Point
+      //-----------------
+      float middleX = this.x + (this.width * 0.5f) - this.tWidth * 0.5f;
+      float middleY = this.y + (this.height * 0.5f) - this.tHeight * 0.5f;
+      //----------------
+      // Calculate initialX and finalX
+      //----------------
+      float initialX = this.x + tWidth * 4f;
+      float lastX = this.x2 - tWidth * 4f;
+      float cX = initialX;
+      //---------------
+      // ADD OBJECTS ON X
+      //---------------
+      while (cX <= lastX)
+      {
+         if (cX < middleX - tWidth || cX > middleX + tWidth)
+            tiles.Add(new Tile(cX, middleY, prefab, layerID));
+         // increase currentX
+         cX += tWidth;
+      }
+      //----------------
+      // Calculate initialY and finalY
+      //----------------
+      float initialY = this.y + tHeight * 4f;
+      float lastY = this.y2 - tHeight * 4f;
+      float cY = initialY;
+      //---------------
+      // ADD OBJECTS ON Y
+      //---------------
+      while (cY <= lastY)
+      {
+         if(cY < middleY - tHeight || cY > middleY + tHeight)
+            tiles.Add(new Tile(middleX, cY, prefab, layerID));
+         // increase currentY
+         cY += tHeight;
+      }
+   }
 }
